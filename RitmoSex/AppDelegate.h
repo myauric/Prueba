281f28d787b6cff7282461747e5dd09546abd29c
Mariella Esther Yauri Castro <Marielita@MacBook-Air-de-Mariella.local>
2013-11-14 19:31:23 -0500

    Initial Commit

---
+++ RitmoSex/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RitmoSex
+//
+//  Created by Mariella Esther Yauri Castro on 14/11/13.
+//  Copyright (c) 2013 mecy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RitmoSex/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RitmoSex
+//
+//  Created by Mariella Esther Yauri Castro on 14/11/13.
+//  Copyright (c) 2013 mecy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
